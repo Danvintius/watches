bb52c619715f11d34e19f635200d429f7123aacb
Danvintius <danila.94@mail.ru>
2024-02-06 23:34:43 +0300

    1

---
+++ src/components/WatchesClass.js
@@ -0,0 +1,8 @@
+export default class WatchesClass {
+    constructor(name, hour, time, id) {
+        this.name = name;
+        this.hour = hour;
+        this.time = time;
+        this.id = id
+    }
+}
